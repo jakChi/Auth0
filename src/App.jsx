0ffb31353995ddda3138e9fb6c063ef31fce6c5f
jakChi <kobachincharauli8@gmail.com>
2023-09-29 22:37:12 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import LoginButton from './components/LoginButton'
+import Logout from './components/Logout'
+import Profile from './components/Profile'
+
+function App() {
+  return (
+    <>
+      <LoginButton />
+      <Logout />
+      <Profile />
+    </>    
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+//import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const domain = "dev-rgojy87ilrg1vkjr.us.auth0.com";
+const clientId = "8oTJR8XDkJ3X9UfzI3vsdBpD8MTPPokD";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
+    <App />
+  </Auth0Provider>,
+)
